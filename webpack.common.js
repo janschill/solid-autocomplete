803c550d97439602c0d00c8ba3deabfe85e1aca0
Jan Schill <janschill@me.com>
2021-04-20 08:40:49 +0200

    Set up project, install dependencies

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    filename: 'solid-autocomplete.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'commonjs2'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ],
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify')
+    }
+  }
+}

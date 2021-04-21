3bb395473304dcd04b60abfd94261dc1efa8be45
Jan Schill <schill@hey.com>
2021-04-21 16:46:08 +0200

    Simple filler

--- src/filler.js
+++ src/filler.js
@@ -1,3 +1,14 @@
 export default class Filler {
+  constructor (params) {
+    this.data = params.data
+    this.tuples = params.tuples
+  }
 
+  fill () {
+    this.tuples.forEach(tuple => {
+      const $input = tuple.input
+      const predicate = tuple.predicate
+      $input.value = this.data[predicate]
+    })
+  }
 }

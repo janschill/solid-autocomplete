424ce4f334c65484561612e297bc68954ecaf1e4
Jan Schill <schill@hey.com>
2021-04-21 16:45:59 +0200

    Remove comments

--- src/picker.js
+++ src/picker.js
@@ -10,9 +10,7 @@ export default class Picker {
     for (let i = 0; i < inputs.length; i++) {
       const $input = inputs[i]
       const $label = this.baseElement.querySelector(`label[for='${$input.id}']`)
-      // const inputId = this.toCamelCase($input.id)
       inputLabelTuples.push({ input: $input, label: $label })
-      // this.filler($input, inputId)
     }
 
     return inputLabelTuples

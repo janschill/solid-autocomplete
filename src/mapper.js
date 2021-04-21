23d77a00661558a80360e913492c1e3c7cde760f
Jan Schill <schill@hey.com>
2021-04-21 16:31:38 +0200

    Add dictionary to Mapper class

--- src/mapper.js
+++ src/mapper.js
@@ -1,2 +1,24 @@
 export default class Mapper {
+  constructor (params) {
+    this.initDictionary()
+  }
+
+  initDictionary () {
+    this.dictionary = {
+      country: 'countryName',
+      countryName: 'countryName',
+      affiliation: 'organizationName',
+      organisation: 'organizationName',
+      organization: 'organizationName',
+      organisationName: 'organizationName',
+      organizationName: 'organizationName',
+      institution: 'organizationName',
+      organizationTitle: 'role',
+      position: 'role',
+      job: 'role',
+      fullName: 'name',
+      fullname: 'name',
+      name: 'name'
+    }
+  }
 }
